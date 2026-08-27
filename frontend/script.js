7fd1630c4f69f35d55b4f6e04509cf7c9a1ee737
Devansh Sharma <devanshs7586@gmail.com>
2026-08-27 20:51:22 +0530

    Initial commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://email-spam-1-98y9.onrender.com";
 
 const emailText = document.querySelector("#emailText");
 const count = document.querySelector("#count");
